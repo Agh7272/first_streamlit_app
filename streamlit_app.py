fae4d3ab461a65f9ba751474d8556ff5e071c859
Agh7272 <127331421+Agh7272@users.noreply.github.com>
2023-03-09 12:55:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import requests
 import pandas
 import snowflake.connector
-from urllib.error import URLErro
+from urllib.error import URLError
 
 streamlit.title("My parents new healthy diner")
 
